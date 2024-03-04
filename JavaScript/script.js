8195997f32600666283e4563def9b37f3768f262
millieddie <w231435.ak@gmail.com>
2024-03-04 18:05:07 +0900

    Revert "Update script.js"
    This reverts commit 78180a0d4cf6b8147a2ae7b6c97c29e499d7ed1d.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -23,27 +23,5 @@ $(function () {
         scrollTop: 0//ページトップまでスクロール
     }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
     return false;//リンク自体の無効化
-  });
-
-  //上部画像の設定
-$('.gallery').slick({
-	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-	fade: true, //フェードの有効化
-	arrows: true,//左右の矢印あり
-	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	asNavFor: '.choice-btn', //連動させるサムネイルのクラス名
 });
-
-//選択画像の設定
-$('.choice-btn').slick({
-	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-	slidesToShow: 4, //表示させるスライドの数
-	focusOnSelect: true, //フォーカスの有効化
-	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	asNavFor: '.gallery', //連動させるスライドショーのクラス名
-});
-
-  
 });
